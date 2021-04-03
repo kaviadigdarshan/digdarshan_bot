2a35861d9efddaacd21588d5e35d421d672f6309
Digdarshan Kavia <kaviadarshan@gmail.com>
2021-04-03 20:44:12 +0200

    stop bot idling

--- bot/main.py
+++ bot/main.py
@@ -27,8 +27,6 @@ elif mode == "prod":
                               port=PORT,
                               url_path=os.getenv("TG_BOT_TOKEN"),
                               webhook_url = f"https://{os.getenv('HEROKU_APP_NAME')}.herokuapp.com/{os.getenv('TG_BOT_TOKEN')}")
-        updater.idle()
-        # updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(os.getenv("HEROKU_APP_NAME"), os.getenv("TG_BOT_TOKEN")))
 else:
     logging.error("No MODE Specified. Please specify mode as one of ['dev', 'prod']")
     sys.exit(1)
